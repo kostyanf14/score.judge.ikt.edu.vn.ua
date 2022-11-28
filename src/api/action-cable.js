1005cc53ae511af3686e4b001e2a955e33504eb9
Kostiantyn Kostiuk <kostyanf14@live.com>
2022-11-28 21:33:17 +0200

    Add autologin via token
    resolves: amnis-invictus/ikt.edu.vn.ua#108
    
    Signed-off-by: Kostiantyn Kostiuk <kostyanf14@live.com>

--- src/api/action-cable.js
+++ src/api/action-cable.js
@@ -6,8 +6,17 @@ import store from '../state';
 
 export const clientId = uuidv4();
 export const task = window.location.pathname.slice(1);
-const password = prompt('Enter judge password');
-const url = `${process.env.REACT_APP_API_ENDPOINT}?password=${password}&task_id=${task}&client_id=${clientId}`;
+let url = `${process.env.REACT_APP_API_ENDPOINT}?task_id=${task}&client_id=${clientId}`;
+
+const currentUrl = new URL(window.location.href);
+const token = currentUrl.searchParams.get("token");
+
+if (!token) {
+  const password = prompt('Enter judge password');
+  url += `&password=${password}`
+} else {
+  url += `&token=${token}`
+}
 const consumer = createConsumer(url);
 
 if (!task) {
