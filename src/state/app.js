039a880e1fc88456c6f2a8dba72958349652a5c6
Kostiantyn Kostiuk <kostyanf14@live.com>
2023-01-20 23:07:02 +0200

    Display contest/task name
    resolve: #94
    
    Signed-off-by: Kostiantyn Kostiuk <kostyanf14@live.com>

--- src/state/app.js
+++ src/state/app.js
@@ -4,10 +4,16 @@ const slice = createSlice({
   name: 'app',
   initialState: {
     isReady: false,
+    contest_name: '',
+    task_name: '',
   },
   reducers: {
     start: (state) => { state.isReady = false },
-    ready: (state) => { state.isReady = true },
+    ready: (state, { payload }) => {
+      state.contest_name = payload.contest_name;
+      state.task_name = payload.task_name;
+      state.isReady = true;
+    },
   }
 });
 
