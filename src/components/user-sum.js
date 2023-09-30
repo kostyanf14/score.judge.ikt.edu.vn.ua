17837cfcda27f314e8a1087304ae830aad220619
Just Me <just806me@gmail.com>
2023-10-01 01:42:27 +0300

    Use result multiplier in the sum calculation

--- src/components/user-sum.js
+++ src/components/user-sum.js
@@ -6,7 +6,12 @@ const UserSum = ({ user }) => {
     s => s.results[user] && Object.values(s.results[user]).map(r => r.value).filter(x => x).map(parseFloat),
     shallowEqual
   );
-  const sum = values?.reduce((x, y) => x + y, 0);
+  const rmValue = useSelector(s => s.resultMultiplier).split('/');
+
+  const rmNumerator = parseInt(rmValue[0]);
+  const rmDenominator = rmValue[1] ? parseInt(rmValue[1]) : 1;
+
+  const sum = values && (values.reduce((x, y) => x + y, 0) * rmNumerator / rmDenominator);
 
   return (
     <div className='input-group'>
